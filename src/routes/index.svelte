9fe2a5b1f27d1b2e604131aed5c2868e22f3f22e
Ori K <orikerergil@gmail.com>
2022-03-10 20:23:36 +0000

    Started prototyping main form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class=" flex justify-center">
+  <div
+    class="pb-8 text-white text-center rounded-lg  m-10  shadow-2xl justify-self-center w-10/12 lg:w-2/4"
+  >
+    <div class="">
+      <h1 class="text-3xl font-bold pt-6">
+        Encoded <span
+          class="before:block before:absolute before:-inset-1 before:skew-x-12 before:skew-y-6 before:bg-pink-400 relative inline-block"
+          ><span
+            class="before:block before:absolute before:-inset-1 before:-skew-x-6 before:skew-y-12 before:bg-cyan-500 relative inline-block"
+            ><span
+              class="before:block before:absolute before:-inset-1  before:skew-y-6 before:bg-pink-500 relative inline-block"
+              ><span class="relative text-white">SHA</span></span
+            ></span
+          ><span /></span
+        >
+      </h1>
+      <input
+        type="text"
+        placeholder="Text..."
+        class="rounded-md shadow-lg placeholder:text-center placeholder:italic mt-4 p-1 w-2/5 text-black"
+      />
+    </div>
+  </div>
+</div>
