80a78f05a4325db29db86f3dd3fb566a9bb94828
Ori K <orikerergil@gmail.com>
2022-03-23 23:56:35 +0000

    Extracted title to another component

---
+++ src/routes/title.svelte
@@ -0,0 +1,13 @@
+<h1 class="text-3xl font-bold pt-6">
+  Encoded <span
+    class="before:block before:absolute before:-inset-1 before:skew-x-12 before:skew-y-6 before:bg-pink-400 relative inline-block"
+    ><span
+      class="before:block before:absolute before:-inset-1 before:-skew-x-6 before:skew-y-12 before:bg-cyan-500 relative inline-block"
+      ><span
+        class="before:block before:absolute before:-inset-1  before:skew-y-6 before:bg-pink-500 relative inline-block"
+      >
+        <span class="relative text-white">SHA</span>
+      </span>
+    </span>
+  </span>
+</h1>
