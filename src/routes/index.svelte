2b2b2bee8ad43f1e8dc62405c206b555dd2018ef
Ori K <orikerergil@gmail.com>
2022-03-23 23:57:06 +0000

    Added backgrounds with dark mode support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,22 @@
-<div class=" flex justify-center">
+<script>
+  import Title from "./title.svelte";
+</script>
+
+<div
+  class=" flex justify-center h-screen bg-center-top
+   bg-[url('/static/bg.jpg')] 
+   dark:bg-[url('/static/bg-dark-1.jpg')] dark:bg-slate-900 "
+>
   <div
-    class="pb-8 text-white text-center rounded-lg  m-10  shadow-2xl justify-self-center w-10/12 lg:w-2/4"
+    class="flex justify-center h-screen w-screen backdrop-blur-lg"
   >
-    <div class="">
-      <h1 class="text-3xl font-bold pt-6">
-        Encoded <span
-          class="before:block before:absolute before:-inset-1 before:skew-x-12 before:skew-y-6 before:bg-pink-400 relative inline-block"
-          ><span
-            class="before:block before:absolute before:-inset-1 before:-skew-x-6 before:skew-y-12 before:bg-cyan-500 relative inline-block"
-            ><span
-              class="before:block before:absolute before:-inset-1  before:skew-y-6 before:bg-pink-500 relative inline-block"
-              ><span class="relative text-white">SHA</span></span
-            ></span
-          ><span /></span
-        >
-      </h1>
+    <div class="h-max justify-self-center pb-8 text-white text-center rounded-lg m-10  shadow-2xl w-11/12 lg:w-2/4  
+    bg-slate-900/60 border-2 border-slate-800/40">
+      <Title />
       <input
         type="text"
         placeholder="Text..."
-        class="rounded-md shadow-lg placeholder:text-center placeholder:italic mt-4 p-1 w-2/5 text-black"
+        class="rounded-md shadow- placeholder:text-center placeholder:italic mt-4 p-1 lg:w-2/5 w-4/5 text-black"
       />
     </div>
   </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+        backgroundPosition: {
+          'center-top': 'center top',
+        }
+    }
   },
   plugins: []
-};
+}
