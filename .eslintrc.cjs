3369efa4bc160acd938419b2386ea58fb989b691
Ori K <orikerergil@gmail.com>
2022-03-27 21:20:05 +0000

    Configured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,12 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		indent: ['error', 4],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+
 	}
 };
