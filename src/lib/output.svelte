e75ded272f84e2a97e9a118aa4eea448523c600b
Ori K <orikerergil@gmail.com>
2022-03-28 11:18:48 +0000

    Fixed theming in output

--- src/lib/output.svelte
+++ src/lib/output.svelte
@@ -4,20 +4,25 @@
 
 <div class="flex justify-center mt-3">
   <div
-    class="p-[0.1rem] bg-gradient-to-tr from-cyan-700 to-pink-700 rounded-l-lg
+    class="p-[0.1rem] 
+    dark:bg-gradient-to-tr dark:from-cyan-700 dark:to-pink-700 rounded-l-lg
     flex
-    max-w-[75%]"
+    max-w-[75%] shadow"
   >
     <span
-      class="bg-slate-800 rounded-l-lg text-slate-400 p-1 pl-2 pr-2 max-w-[100%] text-left break-words"
+      class="bg-cyan-50 dark:bg-slate-800 
+      rounded-l-lg text-slate-400 p-1 pl-2 pr-2 max-w-[100%] text-left break-all font-mono"
       >{input}</span
     >
   </div>
   <div
-    class="flex 
-    p-[0.1rem] bg-gradient-to-br to-cyan-700 from-pink-700 
+    class="flex shadow
+    p-[0.1rem] 
+    dark:bg-gradient-to-br dark:to-cyan-700 dark:from-pink-700 
     rounded-r-lg ml-[-1px]"
   >
-    <button class="bg-slate-800 hover:bg-black/0 rounded-r-lg">Copy</button>
+    <button class="bg-cyan-50 dark:bg-slate-800 
+    text-slate-900 dark:text-slate-400
+    hover:bg-black/0 rounded-r-lg">Copy</button>
   </div>
 </div>
