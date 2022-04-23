7705ce2f65a2101318e0b57ee7fe2b1c53a41c00
OriKerer <orikerergil@gmail.com>
2022-04-23 07:01:56 +0000

    Deploying to gh-pages from @ OriKerer/encoded-sha@14a7a7252e2a40ab32578c7aed248ac096be413e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1020,11 +1020,11 @@ class StaleWhileRevalidate extends Strategy {
   }
 }
 const build = [
-  "/encoded-sha/_app/start-6444543d.js",
+  "/encoded-sha/_app/start-449e20c8.js",
   "/encoded-sha/_app/pages/__layout.svelte-a9500c79.js",
-  "/encoded-sha/_app/assets/pages/__layout.svelte-619480fe.css",
+  "/encoded-sha/_app/assets/pages/__layout.svelte-18f6ae14.css",
   "/encoded-sha/_app/error.svelte-837cd953.js",
-  "/encoded-sha/_app/pages/index.svelte-23915d97.js",
+  "/encoded-sha/_app/pages/index.svelte-70357675.js",
   "/encoded-sha/_app/assets/pages/index.svelte-0f048e4d.css",
   "/encoded-sha/_app/chunks/index-88edda46.js",
   "/encoded-sha/_app/chunks/index-41b5b6b6.js"
@@ -1043,7 +1043,7 @@ const files = [
   "/encoded-sha/img/bg.webp",
   "/encoded-sha/site.webmanifest"
 ];
-const version = "1650693939379";
+const version = "1650697311676";
 precacheAndRoute([
   ...build.map((f) => ({ url: f, revision: null })),
   ...files.filter((f) => !f.includes(".nojekyll")).map((f) => ({ url: f, revision: `${version}` }))
