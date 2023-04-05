99566bdda5e2a9aa4e3d7362a54b80f83f0c420b
Ori Kerer <orikerergil@gmail.com>
2023-04-05 12:52:11 +0300

    Trying to fix Base URL changes in new Sveltekit version

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,7 +10,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : '/encoded-sha',
+      base: '',
     },
   },
 }
